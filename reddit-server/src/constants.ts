6d87dd6f64536e128eebd80c8179ca535d7cdabe
vishwasc20 <vishwasc20@gmail.com>
2021-05-21 20:44:09 +0530

    docker changes

--- reddit-server/src/constants.ts
+++ reddit-server/src/constants.ts
@@ -11,15 +11,22 @@ export const FORGET_PASSWORD_PREFIX = "forget-password:";
 export const VALID_EMAIL_REGEX =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+export const REDIS_URL = process.env.REDIS_URL;
+// ||  "redis-auth-service" : "localhost";
+
+export const SESSION_SECRET = process.env.SESSION_SECRET;
+
+export const CORS_HOST = process.env.CORS_HOST;
+// ? "http://localhost:8080"
+// : "http://localhost:3000";
+
 export const RESET_PASSWORD_TEMPLATE = (token: string) =>
-  `<a href="http://localhost:3000/reset-password/${token}">Reset Password</a>`;
+  `<a href="${CORS_HOST}/reset-password/${token}">Reset Password</a>`;
 
-export const REDIS_HOST = DOCKER_DEV_ENV ? "redis-auth-service" : "localhost";
+export const PORT = parseInt(process.env.PORT) || 4000;
 
-export const CORS_HOST = DOCKER_DEV_ENV
-  ? "http://localhost:8080"
-  : "http://localhost:3000";
+const POSTGRES_HOST = getIPv4Address();
 
-export const PORT = 4000;
-
-export const POSTGRES_HOST = getIPv4Address();
+export const POSTGRES_URL =
+  // process.env.DATABASE_URL ||
+  `postgresql://postgres:postgres@${POSTGRES_HOST}:5432/reddittypeorm`;
--- reddit-server/src/index.ts
+++ reddit-server/src/index.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
+import "dotenv-safe/config";
 import {
   __prod__,
   AUTH_COOKIE_NAME,
   CORS_HOST,
-  REDIS_HOST,
+  REDIS_URL,
   PORT,
-  POSTGRES_HOST,
+  POSTGRES_URL,
+  SESSION_SECRET,
 } from "./constants";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -28,23 +30,21 @@ import { createUpdootLoader } from "./utils/createUpdootLoader";
 const main = async () => {
   const typeORMConnection = await createConnection({
     type: "postgres",
-    database: "reddittypeorm",
-    username: "postgres",
-    password: "postgres",
+    url: POSTGRES_URL,
     logging: !__prod__,
     synchronize: !__prod__,
     entities: [Post, User, Updoot],
-    host: POSTGRES_HOST,
     migrations: [path.join(__dirname, "./migrations/*")],
   });
   await typeORMConnection.runMigrations();
 
+  // To delete all posts clean
   // await Post.delete({});
 
   const app = express();
 
   const RedisStore = connectRedis(session);
-  const redis = new Redis({ host: REDIS_HOST });
+  const redis = new Redis(REDIS_URL);
   app.use(
     cors({
       origin: CORS_HOST,
@@ -63,9 +63,10 @@ const main = async () => {
         httpOnly: true,
         sameSite: "lax", // csrf
         secure: __prod__, // cookie only works in https
+        // domain: __prod__ ? "custom-domain": undefined
       },
       saveUninitialized: false,
-      secret: "qowiueojwojfalksdjoqiwueo",
+      secret: SESSION_SECRET,
       resave: false,
     })
   );
